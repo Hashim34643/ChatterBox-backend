a47f1c3e130e60d04ed7ad4bd8504c60a7a7135d
Hashim34643 <hashimhussain7312@gmail.com>
2024-05-10 16:35:27 +0100

    room model created

---
+++ models/room.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Room = mongoose.model('Room', roomSchema);
+
+module.exports = Room;
