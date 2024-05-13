65a6e5789d3bad4db948c22b6a8a390ddef54e0f
Hashim34643 <hashimhussain7312@gmail.com>
2024-05-13 16:35:16 +0100

    update room router made

--- routes/update-room.js
+++ routes/update-room.js
@@ -4,6 +4,6 @@ const isAuth = require('../middleware/auth');
 
 const updateRoomRouter = express.Router();
 
-updateRoomRouter.patch('/:roomId', isAuth, roomController);
+updateRoomRouter.patch('/room/update/:roomId', isAuth, roomController);
 
 module.exports = updateRoomRouter;
