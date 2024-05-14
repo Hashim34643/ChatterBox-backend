b74e60f7446bed32edbcad8a70023a8d48241b07
Hashim34643 <hashimhussain7312@gmail.com>
2024-05-14 16:06:18 +0100

    message model created

---
+++ models/message.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  roomId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
