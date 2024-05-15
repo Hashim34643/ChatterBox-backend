d344432d8498c159c72d3d48d9715eed93f2f8bc
Hashim34643 <hashimhussain7312@gmail.com>
2024-05-15 16:23:43 +0100

    edit message controller file created

---
+++ controllers/edit-message.js
@@ -0,0 +1,25 @@
+const Message = require('../models/message');
+
+const editMessage = async (req, res) => {
+  try {
+    const { messageId } = req.params;
+    const { content } = req.body;
+
+    const message = await Message.findById(messageId);
+    if (!message) {
+      return res.status(404).json({ success: false, message: 'Message not found' });
+    }
+
+    message.content = content;
+    message.edited = true;
+
+    const updatedMessage = await message.save();
+
+    res.status(200).json({ success: true, message: 'Message edited successfully', message: updatedMessage });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Error editing message', error: error.message });
+  }
+};
+
+module.exports = editMessage;
